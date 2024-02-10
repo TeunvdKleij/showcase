847ec50a8393ab972e338a8d229f7518ee9de169
Teunvdk <33592768+TonyChocolony@users.noreply.github.com>
2024-02-10 12:50:27 +0100

    contact form included/cookie

---
+++ javascript/currectSelected.js
@@ -0,0 +1,47 @@
+class CurrentSelected{
+    constructor(){
+
+        this.bindEvents();
+    }
+
+    bindEvents() {
+        let buttonProfile = document.querySelector('#profile-button');
+        buttonProfile.addEventListener('click', () => {
+            this.showSection('profile');
+        });
+        let buttonContactForm = document.querySelector('#contact-form-button');
+        buttonContactForm.addEventListener('click', () => {
+            this.showSection('contactForm');
+        });
+    }
+    showSection(sectionName){
+        document.querySelector(`#profile-button`).classList.remove('profile-button__current');
+        document.querySelector(`#contact-form-button`).classList.remove('profile-button__current');
+            
+        if(sectionName === 'profile'){
+            document.querySelector(`#profile-button`).classList.add('profile-button__current');
+            document.querySelector('.profile-content').classList.remove('hide');
+            document.querySelector('.profile-content').classList.add('show');
+            document.querySelector('.contact-form-content').classList.add('hide');
+            document.querySelector('.contact-form-content').classList.remove('show');
+        }
+        else if(sectionName === 'contactForm'){
+            document.querySelector(`#contact-form-button`).classList.add('profile-button__current');
+            document.querySelector('.profile-content').classList.add('hide');
+            document.querySelector('.profile-content').classList.remove('show');
+            document.querySelector('.contact-form-content').classList.remove('hide');
+            document.querySelector('.contact-form-content').classList.add('show');
+
+        }
+    }
+
+    hideProfile(){
+        document.querySelector(`#profile-button`).classList.remove('profile-button__current');
+    }
+    hideContactForm(){
+        document.querySelector(`#contact-form-button`).classList.remove('profile-button__current');
+    }
+
+}
+
+const currentSelected = new CurrentSelected();

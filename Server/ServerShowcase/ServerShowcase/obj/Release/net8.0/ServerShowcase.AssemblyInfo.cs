6232a9fb324cb97534f77a48125a957691bc5540
Teunvdk <33592768+TonyChocolony@users.noreply.github.com>
2024-02-23 11:43:09 +0100

    commit

--- Server/ServerShowcase/ServerShowcase/obj/Release/net8.0/ServerShowcase.AssemblyInfo.cs
+++ Server/ServerShowcase/ServerShowcase/obj/Release/net8.0/ServerShowcase.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("b1b30cc4-aebd-48f3-a487-7e4471f75a04")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerShowcase")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a71e3dfd1093807e2c6639a80cee57e463a2289")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerShowcase")]
